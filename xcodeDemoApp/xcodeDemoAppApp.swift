1a4bdd3a00547645ce026136c7fa32753da5acc9
VulpesFerrilata014 <tts0104@yonsei.ac.kr>
2024-06-08 13:56:45 +0900

    Initial Commit

---
+++ xcodeDemoApp/xcodeDemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  xcodeDemoAppApp.swift
+//  xcodeDemoApp
+//
+//  Created by snlcom on 6/8/24.
+//
+
+import SwiftUI
+
+@main
+struct xcodeDemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
